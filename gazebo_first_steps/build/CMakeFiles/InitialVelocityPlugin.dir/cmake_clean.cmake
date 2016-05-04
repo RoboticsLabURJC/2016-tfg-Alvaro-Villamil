06a464aa07cc62865c826549eb1bd9853cd36d8a
Alvaro Villamil <kb8_alvaro@hotmail.com>
2016-05-04 13:25:11 +0200

    First steps in gazebo and plugins, create worlds and objects

---
+++ gazebo_first_steps/build/CMakeFiles/InitialVelocityPlugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/InitialVelocityPlugin.dir/InitialVelocityPlugin.cc.o"
+  "libInitialVelocityPlugin.pdb"
+  "libInitialVelocityPlugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/InitialVelocityPlugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ gazebo_first_steps/build/CMakeFiles/world_edit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/world_edit.dir/world_edit.cc.o"
+  "libworld_edit.pdb"
+  "libworld_edit.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/world_edit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
