946fd36e08c92dfe4b75e4756959affd2b1c8314
Alvaro Villamil <kb8_alvaro@hotmail.com>
2016-05-23 21:20:53 +0200

    Added moving_objects folder. Create an empty world with a cylinder.
    next step: move the cylinder

---
+++ moving_objects/build/CMakeFiles/step1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/step1.dir/step1.cc.o"
+  "libstep1.pdb"
+  "libstep1.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/step1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
