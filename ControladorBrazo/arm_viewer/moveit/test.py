b656e4569bddca348d8ab6c99338f0da79ac490a
Alvaro Villamil <kb8_alvaro@hotmail.com>
2017-06-28 18:50:40 +0200

    Finnished chapters 1, 3 and 4. Starting 5 and 2

--- ControladorBrazo/arm_viewer/moveit/test.py
+++ ControladorBrazo/arm_viewer/moveit/test.py
@@ -1,87 +1,44 @@
 #!/usr/bin/env python
 
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2013, SRI International
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of SRI International nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Acorn Pooley
-
-## BEGIN_SUB_TUTORIAL imports
-##
-## To use the python interface to move_group, import the moveit_commander
-## module.  We also import rospy and some messages that we will use.
 import sys
 import copy
 import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
-## END_SUB_TUTORIAL
 
 from std_msgs.msg import String
 
 def move_group_python_interface_tutorial():
-  ## BEGIN_TUTORIAL
-  ##
-  ## Setup
-  ## ^^^^^
-  ## CALL_SUB_TUTORIAL imports
-  ##
   ## First initialize moveit_commander and rospy.
   print "============ Starting tutorial setup"
   moveit_commander.roscpp_initialize(sys.argv)
-  rospy.init_node('move_group_python_interface_tutorial',
-                  anonymous=True)
+  print "============ Started commander"
+  rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
+  print "============ Started node"
 
   ## Instantiate a RobotCommander object.  This object is an interface to
   ## the robot as a whole.
   robot = moveit_commander.RobotCommander()
+  print "============ Started robot"
 
   ## Instantiate a PlanningSceneInterface object.  This object is an interface
   ## to the world surrounding the robot.
   scene = moveit_commander.PlanningSceneInterface()
+  print "============ Started scene"
 
   ## Instantiate a MoveGroupCommander object.  This object is an interface
   ## to one group of joints.  In this case the group is the joints in the left
   ## arm.  This interface can be used to plan and execute motions on the left
   ## arm.
-  group = moveit_commander.MoveGroupCommander("gear")
+  group = moveit_commander.MoveGroupCommander("manipulator")
+  print "============ Started group"
 
 
   ## We create this DisplayTrajectory publisher which is used below to publish
   ## trajectories for RVIZ to visualize.
-  display_trajectory_publisher = rospy.Publisher(
-                                      '/move_group/display_planned_path',
-                                      moveit_msgs.msg.DisplayTrajectory,
-                                      queue_size=20)
+  display_trajectory_publisher = rospy.Publisher( '/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=20)
+  print "============ Started rviz dispay"
 
   ## Wait for RVIZ to initialize. This sleep is ONLY to allow Rviz to come up.
   print "============ Waiting for RVIZ..."
@@ -100,11 +57,19 @@ def move_group_python_interface_tutorial():
   ## We can get a list of all the groups in the robot
   print "============ Robot Groups:"
   print robot.get_group_names()
+  print "============ Robot Joints:"
+  print robot.get_joint_names()
 
   ## Sometimes for debugging it is useful to print the entire state of the
   ## robot.
   print "============ Printing robot state"
   print robot.get_current_state()
+  print "============"
+  print "============ Printing joint values"
+  print group.get_current_joint_values()
+  print "============"
+  print "============ Printing pose"
+  print group.get_current_pose()
   print "============"
 
 
@@ -155,9 +120,6 @@ def move_group_python_interface_tutorial():
   ## a blocking function and requires a controller to be active
   ## and report success on execution of a trajectory.
 
-  # Uncomment below line when working with a real robot
-  # group.go(wait=True)
-
   ## Planning to a joint-space goal 
   ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   ##
@@ -172,8 +134,8 @@ def move_group_python_interface_tutorial():
 
   ## Now, let's modify one of the joints, plan to the new joint
   ## space goal and visualize the plan
-  group_variable_values[0] = 1.0
-  group.set_joint_value_target(group_variable_values)
+##  group_variable_values[0] = 1.0
+##  group.set_joint_value_target(group_variable_values)
 
   plan2 = group.plan()
 
@@ -181,55 +143,8 @@ def move_group_python_interface_tutorial():
   rospy.sleep(5)
 
 
-  ## Cartesian Paths
-  ## ^^^^^^^^^^^^^^^
-  ## You can plan a cartesian path directly by specifying a list of waypoints 
-  ## for the end-effector to go through.
-  waypoints = []
-
-  # start with the current pose
-  waypoints.append(group.get_current_pose().pose)
-
-  # first orient gripper and move forward (+x)
-  wpose = geometry_msgs.msg.Pose()
-  wpose.orientation.w = 1.0
-  wpose.position.x = waypoints[0].position.x + 0.1
-  wpose.position.y = waypoints[0].position.y
-  wpose.position.z = waypoints[0].position.z
-  waypoints.append(copy.deepcopy(wpose))
-
-  # second move down
-  wpose.position.z -= 0.10
-  waypoints.append(copy.deepcopy(wpose))
-
-  # third move to the side
-  wpose.position.y += 0.05
-  waypoints.append(copy.deepcopy(wpose))
-
-  ## We want the cartesian path to be interpolated at a resolution of 1 cm
-  ## which is why we will specify 0.01 as the eef_step in cartesian
-  ## translation.  We will specify the jump threshold as 0.0, effectively
-  ## disabling it.
-  (plan3, fraction) = group.compute_cartesian_path(
-                               waypoints,   # waypoints to follow
-                               0.01,        # eef_step
-                               0.0)         # jump_threshold
-                               
-  print "============ Waiting while RVIZ displays plan3..."
-  rospy.sleep(5)
-
- 
-  ## Adding/Removing Objects and Attaching/Detaching Objects
-  ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  ## First, we will define the collision object message
-  collision_object = moveit_msgs.msg.CollisionObject()
-
-
-
   ## When finished shut down moveit_commander.
   moveit_commander.roscpp_shutdown()
-
-  ## END_TUTORIAL
 
   print "============ STOPPING"
 
